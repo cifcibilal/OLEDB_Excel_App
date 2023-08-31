938f9d38dae780e4b076c93b0e64355975092975
cifcibilal <cifci.bilal@outlook.com>
2023-08-31 16:47:51 +0300

    Proje dosyası ekle.

---
+++ OLEDB_Excel_App/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace OLEDB_Excel_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.comboBoxSheets = new System.Windows.Forms.ComboBox();
+            this.textBoxFilter = new System.Windows.Forms.TextBox();
+            this.btnApplyFilter = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(980, 415);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 18);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(974, 394);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.btnApplyFilter);
+            this.groupBox2.Controls.Add(this.textBoxFilter);
+            this.groupBox2.Controls.Add(this.comboBoxSheets);
+            this.groupBox2.Controls.Add(this.btnSelectFile);
+            this.groupBox2.Location = new System.Drawing.Point(15, 434);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(977, 143);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(19, 21);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(117, 47);
+            this.btnSelectFile.TabIndex = 0;
+            this.btnSelectFile.Text = "Dosya Seç";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // comboBoxSheets
+            // 
+            this.comboBoxSheets.FormattingEnabled = true;
+            this.comboBoxSheets.Location = new System.Drawing.Point(19, 102);
+            this.comboBoxSheets.Name = "comboBoxSheets";
+            this.comboBoxSheets.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxSheets.TabIndex = 1;
+            this.comboBoxSheets.SelectedIndexChanged += new System.EventHandler(this.comboBoxSheets_SelectedIndexChanged);
+            // 
+            // textBoxFilter
+            // 
+            this.textBoxFilter.Location = new System.Drawing.Point(302, 59);
+            this.textBoxFilter.Name = "textBoxFilter";
+            this.textBoxFilter.Size = new System.Drawing.Size(487, 22);
+            this.textBoxFilter.TabIndex = 2;
+            // 
+            // btnApplyFilter
+            // 
+            this.btnApplyFilter.Location = new System.Drawing.Point(820, 21);
+            this.btnApplyFilter.Name = "btnApplyFilter";
+            this.btnApplyFilter.Size = new System.Drawing.Size(117, 60);
+            this.btnApplyFilter.TabIndex = 3;
+            this.btnApplyFilter.Text = "Filtrele";
+            this.btnApplyFilter.UseVisualStyleBackColor = true;
+            this.btnApplyFilter.Click += new System.EventHandler(this.btnApplyFilter_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(299, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Select * From Sheet Where ...";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Sayfa Seç";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1004, 585);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.ComboBox comboBoxSheets;
+        private System.Windows.Forms.Button btnApplyFilter;
+        private System.Windows.Forms.TextBox textBoxFilter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ OLEDB_Excel_App/Form1.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.IO;
+using Excel = Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Interop.Excel;
+
+namespace OLEDB_Excel_App
+{
+    public partial class Form1 : Form
+    {
+        string filePath;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            filePath = DosyaAcVePathBul();
+
+            OleDbConnection oleDbConnection = new OleDbConnection($@"Provider=Microsoft.ACE.OLEDB.16.0;Data Source={filePath}; Extended Properties='Excel 12.0 xml;HDR=YES;'");
+            oleDbConnection.Open();
+
+            System.Data.DataTable sheets = oleDbConnection.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+
+            comboBoxSheets.Items.Clear();
+            foreach (DataRow sheet in sheets.Rows)
+            {
+                string sheetName = sheet["TABLE_NAME"].ToString();
+                comboBoxSheets.Items.Add(sheetName);
+            }
+            if (comboBoxSheets.Items.Count > 0)
+                comboBoxSheets.SelectedIndex = 0;
+            /*
+            OleDbDataAdapter oleDbDataAdapter = new OleDbDataAdapter("SELECT * FROM [Data$]",oleDbConnection);
+
+            DataTable dataTable = new DataTable();
+
+            oleDbDataAdapter.Fill(dataTable);
+
+            dataGridView1.DataSource = dataTable.DefaultView;
+            */
+            oleDbConnection.Close();
+
+        }
+        public string DosyaAcVePathBul()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            openFileDialog.Filter = "Excel Dosyası |*.xlsx| Excel Dosyası|*.xls";
+            openFileDialog.ShowDialog();
+            groupBox1.Text = openFileDialog.FileName;
+
+            return groupBox1.Text.ToString();
+        }
+
+        private void comboBoxSheets_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedSheet = comboBoxSheets.SelectedItem.ToString();
+
+            OleDbConnection oleDbConnection = new OleDbConnection($@"Provider=Microsoft.ACE.OLEDB.16.0;Data Source={groupBox1.Text}; Extended Properties='Excel 12.0 xml;HDR=YES;'");
+            oleDbConnection.Open();
+
+            OleDbDataAdapter oleDbDataAdapter = new OleDbDataAdapter($"SELECT * FROM [{selectedSheet}]", oleDbConnection);
+
+            System.Data.DataTable dataTable = new System.Data.DataTable();
+
+            oleDbDataAdapter.Fill(dataTable);
+
+            dataGridView1.DataSource = dataTable.DefaultView;
+
+            oleDbConnection.Close();
+        }
+
+        private void btnApplyFilter_Click(object sender, EventArgs e)
+        {
+            string filterQuery = textBoxFilter.Text;
+
+            if (string.IsNullOrEmpty(filterQuery))
+            {
+                MessageBox.Show("Lütfen bir filtre sorgusu girin.");
+                return;
+            }
+            
+            string selectedSheet = comboBoxSheets.SelectedItem.ToString();
+
+            OleDbConnection oleDbConnection = new OleDbConnection($@"Provider=Microsoft.ACE.OLEDB.16.0;Data Source={groupBox1.Text}; Extended Properties='Excel 12.0 xml;HDR=YES;'");
+            oleDbConnection.Open();
+
+            OleDbDataAdapter oleDbDataAdapter = new OleDbDataAdapter($"SELECT * FROM [{selectedSheet}] WHERE {filterQuery}", oleDbConnection);
+            
+            //OleDbDataAdapter oleDbDataAdapter = new OleDbDataAdapter($"{filterQuery}", oleDbConnection);
+
+            System.Data.DataTable dataTable = new System.Data.DataTable();
+
+            oleDbDataAdapter.Fill(dataTable);
+
+            dataGridView1.DataSource = dataTable.DefaultView;
+
+            oleDbConnection.Close();
+
+        }
+        //tekrar incelenmek üzere silinmedi.
+       /*
+        * private void btnSaveAs_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.DataSource == null)
+            {
+                MessageBox.Show("Önce bir sorgu sonucu alın.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string excelFilePath = groupBox1.Text; // Excel dosyasının yolu ve adı
+            string worksheetName = "sql"; // Sayfa adı
+
+            try
+            {
+                Excel.Application excelApp = new Excel.Application();
+                Excel.Workbook workbook = excelApp.Workbooks.Open(excelFilePath);
+                Excel._Worksheet worksheet = null;
+
+                foreach (Excel._Worksheet sheet in workbook.Sheets)
+                {
+                    if (sheet.Name == worksheetName)
+                    {
+                        worksheet = sheet;
+                        break;
+                    }
+                }
+
+                if (worksheet == null)
+                {
+                    MessageBox.Show("Sayfa bulunamadı.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    workbook.Close();
+                    excelApp.Quit();
+                    return;
+                }
+
+                DataView queryResultView = (DataView)dataGridView1.DataSource;
+                System.Data.DataTable queryResultTable = queryResultView.ToTable();
+
+                int row = worksheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell).Row + 1;
+
+                for (int i = 0; i < queryResultTable.Rows.Count; i++)
+                {
+                    for (int j = 0; j < queryResultTable.Columns.Count; j++)
+                    {
+                        worksheet.Cells[row + i, j + 1] = queryResultTable.Rows[i][j].ToString();
+                    }
+                }
+
+                workbook.Save();
+                workbook.Close();
+                excelApp.Quit();
+
+                MessageBox.Show("İşlem Başarılı.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata oluştu: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        /*
+        if (dataGridView1.DataSource == null)
+        {
+            MessageBox.Show("Önce bir sorgu sonucu alın.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            return;
+        }
+
+        SaveFileDialog saveFileDialog = new SaveFileDialog();
+        saveFileDialog.Filter = "Excel Dosyası |*.xlsx";
+        saveFileDialog.Title = "Sorgu Sonucunu Excel'e Kaydet";
+        saveFileDialog.ShowDialog();
+
+        if (saveFileDialog.FileName != "")
+        {
+            try
+            {
+                DataView queryResultView = (DataView)dataGridView1.DataSource;
+                DataTable queryResultTable = queryResultView.ToTable();
+
+                using (OleDbConnection oleDbConnection = new OleDbConnection($@"Provider=Microsoft.ACE.OLEDB.16.0;Data Source={saveFileDialog.FileName}; Extended Properties='Excel 12.0 xml;HDR=YES;'"))
+                {
+                    oleDbConnection.Open();
+
+                    string tableName = "Sorgu Sonucu";
+                    string createTableQuery = $"CREATE TABLE [{tableName}] (";
+
+                    createTableQuery += "[EEID] INTEGER,";
+                    createTableQuery += "[Full Name] TEXT,";
+                    createTableQuery += "[Job Title] TEXT,";
+                    createTableQuery += "[Department] TEXT,";
+                    createTableQuery += "[Business Unit] TEXT,";
+                    createTableQuery += "[Gender] TEXT,";
+                    createTableQuery += "[Ethnicity] TEXT,";
+                    createTableQuery += "[Age] INTEGER,";
+                    createTableQuery += "[Hire Date] DATETIME,";
+                    createTableQuery += "[Annual Salary] DOUBLE,";
+                    createTableQuery += "[Bonus %] DOUBLE,";
+                    createTableQuery += "[Country] TEXT,";
+                    createTableQuery += "[City] TEXT,";
+                    createTableQuery += "[Exit Date] DATETIME)";
+
+                    //createTableQuery = createTableQuery.TrimEnd(',', ' ') + ")";
+                    using (OleDbCommand createTableCommand = new OleDbCommand(createTableQuery, oleDbConnection))
+                    {
+                        createTableCommand.ExecuteNonQuery();
+                    }
+
+                    foreach (DataRow row in queryResultTable.Rows)
+                    {
+                        string insertQuery = $"INSERT INTO [{tableName}] ([EEID], [Full Name], [Job Title], [Department], [Business Unit], [Gender], [Ethnicity], [Age], [Hire Date], [Annual Salary], [Bonus %], [Country], [City], [Exit Date]) VALUES (";
+
+                        for (int i = 0; i < row.ItemArray.Length; i++)
+                        {
+                            object item = row.ItemArray[i];
+
+                            if (queryResultTable.Columns[i].DataType == typeof(int))
+                            {
+                                insertQuery += $"{item}, ";
+                            }
+                            else if (queryResultTable.Columns[i].DataType == typeof(double))
+                            {
+                                insertQuery += $"{item.ToString().Replace(',', '.')}, ";
+                            }
+                            else if (queryResultTable.Columns[i].DataType == typeof(DateTime))
+                            {
+                                DateTime dateValue = (DateTime)item;
+                                insertQuery += $"'{dateValue.ToString("yyyy-MM-dd HH:mm:ss")}', ";
+                            }
+                            else
+                            {
+                                insertQuery += $"'{item.ToString().Replace("'", "''")}', ";
+                            }
+                        }
+
+                        insertQuery = insertQuery.TrimEnd(',', ' ') + ")";
+
+                        using (OleDbCommand insertCommand = new OleDbCommand(insertQuery, oleDbConnection))
+                        {
+                            insertCommand.ExecuteNonQuery();
+                        }
+                    }
+
+                    oleDbConnection.Close();
+
+                    MessageBox.Show("İşlem Başarılı.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata oluştu: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        */
+
+    }
+
+}
+
+
